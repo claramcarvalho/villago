631816014f8c4d1d0fd5f41c08ca664498ee5ab9
bonnieives <bonnieives@gmail.com>
2023-11-19 19:29:08 -0500

    "styled the information tabs"

--- public/index.js
+++ public/index.js
@@ -12,3 +12,35 @@ function closeForm() {
 
 }
 
+function activatePersonalTab() {
+    
+    const personal = document.getElementById("personalPage");
+    const serviceProvider = document.getElementById("serviceProviderPage");
+    const culturalPromoter = document.getElementById("culturalPromoterPage");
+
+    personal.style.display = "block";
+    serviceProvider.style.display = "none";
+    culturalPromoter.style.display = "none";
+}
+
+function activateServiceProviderTab() {
+    
+    const personal = document.getElementById("personalPage");
+    const serviceProvider = document.getElementById("serviceProviderPage");
+    const culturalPromoter = document.getElementById("culturalPromoterPage");
+    
+    personal.style.display = "none";
+    serviceProvider.style.display = "block";
+    culturalPromoter.style.display = "none";
+}
+
+function activateCulturalPromoterTab() {
+    
+    const personal = document.getElementById("personalPage");
+    const serviceProvider = document.getElementById("serviceProviderPage");
+    const culturalPromoter = document.getElementById("culturalPromoterPage");
+    
+    personal.style.display = "none";
+    serviceProvider.style.display = "none";
+    culturalPromoter.style.display = "block";
+}
